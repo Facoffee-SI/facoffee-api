cd553a0707d266f2ad57c90f782a9072c98bcfb3
anniasebold <annia.almeida@clubbi.com.br>
2024-06-28 18:42:29 -0400

    feat: documentação swagger

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,7 +1,10 @@
 import { Controller, Get } from '@nestjs/common';
+import { ApiOperation, ApiTags } from '@nestjs/swagger';
 
 @Controller()
+@ApiTags('health-check')
 export class AppController {
+  @ApiOperation({ summary: 'Verifica se o servidor está online.' })
   @Get()
   getHealthCheck() {
     return JSON.stringify({ status: 'up' });
